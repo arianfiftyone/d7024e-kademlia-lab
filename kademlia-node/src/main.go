947af97195c68a1c1ba1daf322b71b094d4a87fb
arianfiftyone <arian.masoudi@hotmail.com>
2023-09-06 15:41:37 +0200

    Added NODE_PORT as env variable and handled as such

--- kademlia-node/src/main.go
+++ kademlia-node/src/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/arianfiftyone/src/kademlia"
 )
 
 func main() {
+
+	NODE_PORT_STR := os.Getenv("NODE_PORT")
+	NODE_PORT, _ := strconv.Atoi(NODE_PORT_STR)
+
 	fmt.Printf("Starting node...\n")
 
-	err := kademlia.Listen("", 3000)
+	err := kademlia.Listen("", NODE_PORT)
 	if err != nil {
 		panic(err)
 
