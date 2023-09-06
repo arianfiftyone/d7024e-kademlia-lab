2d35d82b0bae061e075359085b9459d2b6e1e4d5
arianfiftyone <arian.masoudi@hotmail.com>
2023-09-06 12:31:50 +0200

    Added a kademlia instance, depending on whether node is bootstrap or not.

--- kademlia-node/src/kademlia/kademlia.go
+++ kademlia-node/src/kademlia/kademlia.go
@@ -1,6 +1,55 @@
 package kademlia
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
 type Kademlia struct {
+	rt *RoutingTable
+}
+
+const (
+	BootstrapKademliaID = "FFFFFFFF00000000000000000000000000000000"
+)
+
+func NewKademliaInstance() *Kademlia {
+
+	var routingTable *RoutingTable
+	var kademliaID KademliaID
+
+	envValue := os.Getenv("IS_BOOTSTRAP")
+	// converting string to bool
+	IS_BOOTSTRAP := strings.ToLower(envValue) == "true"
+	hostname := os.Getenv("CONTAINER_NODE")
+
+	localIps, err := net.LookupIP(hostname)
+	if err != nil {
+		// TODO: Handle the error more gracefully(log or return)
+		fmt.Println("Error", err)
+	}
+	localIp := localIps[0].String()
+
+	if IS_BOOTSTRAP {
+
+		kademliaID = *NewKademliaID(BootstrapKademliaID)
+	} else {
+
+		kademliaID = *NewRandomKademliaID()
+	}
+
+	// Create a new Contact instance based on the Kademlia ID and local IP
+	contact := NewContact(&kademliaID, localIp)
+
+	// Create a new RoutingTable instance and add the initial contact
+	routingTable = NewRoutingTable(contact)
+
+	return &Kademlia{
+		rt: routingTable,
+	}
+
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
